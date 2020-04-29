4c816f683bf960fcee539a8de0af8c42a46c216d
Iamjaya <jaya.nitp.cse@gmail.com>
2020-04-29 10:57:25 +0530

    Updated files after signup view,CSRF& UserModel

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser =require('body-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose =require('mongoose');
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
