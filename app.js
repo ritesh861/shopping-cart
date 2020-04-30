6d34790cd732d4dd87a3e808f1310330c2c61f61
ritesh <riteshsingh861@gmail.com>
2020-04-30 07:47:57 +0530

    Till Email and password validation done

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose =require('mongoose');
 var session = require('express-session');
-var indexRouter = require('./routes/index');
+var passport=require('passport');
+var flash =require('connect-flash');
+var indexRouter = require('./routes/index.js');
+var validator=require('express-validator');
 //var usersRouter = require('./routes/users');
 
 var app = express();
@@ -16,7 +19,7 @@ var app = express();
 
 //had to use the below connection function for mongoose. it is diff from the video (to avoid error).
 mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true ,useUnifiedTopology: true});
-
+require('./config/passport');
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs',expressHbs({extname:'.hbs',defaultLayout : 'layout',layoutsDir: path.join(__dirname,'views/layouts'),
@@ -24,10 +27,17 @@ partialDir: [ path.join(__dirname,'views/partials')]}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
+
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());//can google passport strategies for more info.
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
