0acade6f157fce892d102cfeb8517bafa7b9bd4d
ritesh <riteshsingh861@gmail.com>
2020-05-05 00:05:55 +0530

    created add to cart page and checkout page

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,18 @@ router.get('/add-to-cart/:id', function(req,res, next){
     res.redirect('/');
   });
 });
+router.get('/shopping-cart',function(req,res,next){
+  if(!req.session.cart){
+    return res.render('shop/shopping-cart',{products: null});
+  }
+  var  cart=new Cart(req.session.cart);
+  res.render('shop/shopping-cart',{products:cart.generateArray(),totalPrice:cart.totalPrice});
+});
+router.get('/checkout',function(req,res,next){
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart');
+  }
+  var cart=new Cart(req.session.cart);
+  res.render('shop/checkout',{total:cart.totalPrice});
+});
 module.exports = router;
