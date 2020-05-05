d651da54a24debaa870dc6766690c24bc21824e2
ritesh <riteshsingh861@gmail.com>
2020-05-05 21:09:48 +0530

    used stripe for checkout

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find(function(err, docs){
     var productChunks = [];
     var chunkSize = 3;
     for(var i= 0;i<docs.length;i+=chunkSize){
       productChunks.push(docs.slice(i,i+chunkSize));
     }
-    res.render('shop/index', { title: 'Shopping Cart' , products:productChunks });
+    res.render('shop/index', { title: 'Shopping Cart' , products:productChunks , successMsg: successMsg , noMessages: !successMsg });
   });
 
 });
@@ -40,6 +41,37 @@ router.get('/checkout',function(req,res,next){
     return res.redirect('/shopping-cart');
   }
   var cart=new Cart(req.session.cart);
-  res.render('shop/checkout',{total:cart.totalPrice});
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout',{total:cart.totalPrice , errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout' ,function(req,res,next)
+{
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart');
+  }
+  var cart=new Cart(req.session.cart);
+  var stripe = require('stripe')('sk_test_VotpUQk6resLdTRGXBSoDhzo00VlZe8KZ4');
+
+// `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+stripe.charges.create(
+  {
+    amount: cart.totalPrice = 100 ,
+    currency: 'usd',
+    source: req.body.stripeToken,
+    description: 'My First Test Charge',
+  },
+  function(err, charge) {
+    // asynchronously called
+    if(err)
+    {
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
+    }
+    req.flash('success' , ' COOL DUDE ;) ,Successfully Bought Product!!');
+    req.cart= null;
+    res.redirect ('/');
+  }
+);
 });
 module.exports = router;
